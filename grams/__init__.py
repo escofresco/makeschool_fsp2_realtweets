07dae0c244b4eaa2648a4afac5ef9435a798ae1b
Jonasz Karwacki <jonaszakr@gmail.com>
2019-12-12 19:19:21 -0800

    add nltk_data when grams is initialized

--- grams/__init__.py
+++ grams/__init__.py
@@ -1,4 +1,11 @@
+import os
+
+import nltk
+
 from .grams import *
 from .online import *
 from .stats import *
 from .utils import *
+
+# add the `nltk_data` folder to nltk's list of directories to find data in
+nltk.data.path.append(os.path.join(os.getcwd(), "grams/nltk_data/"))
