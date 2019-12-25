763f540a8ca9886b631113ef38f4a9b7244653c0
Jonasz Karwacki <jonaszakr@gmail.com>
2019-12-24 18:42:50 -0800

    update with renamed classes

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask
 
 from grams.grams import Histogram
+from grams.markov import MC
 
 app = Flask(__name__)
 
 with open("res/the_adventures_of_sherlock_holmes.txt") as f:
-    histogram = Histogram(f)
+    #histogram = Histogram(f)
+    markovmodel = MC(str(f.read()))
 
 
 @app.route("/")
 def home():
-    return histogram.sample()
+    #return histogram.sample()
+    return markovmodel.generate(10)
 
 
 if __name__ == "__main__":
