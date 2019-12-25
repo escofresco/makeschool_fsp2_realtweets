134be14e6efa87ae028ca0d79977292aabc48801
Jonasz Karwacki <jonaszakr@gmail.com>
2019-12-25 02:05:00 -0800

    add process for generating markov chain

--- app.py
+++ app.py
@@ -1,23 +1,58 @@
-from flask import Flask
+import marshal
+from multiprocessing import Condition, Process, Queue, Pipe
+from threading import Timer
+from types import FunctionType
+import pickle
+
+from flask import Flask, url_for
 
 from grams.grams import Histogram
 from grams.markov import MC
 
-app = Flask(__name__)
 
-# with open("res/the_adventures_of_sherlock_holmes.txt") as f:
-#     markovmodel = MC(str(f.read()))
-s = ("""If you need Celery to be able to store the results of tasks, you’ll need
-     to choose a result store. If not, skip to the next section. Characteristics that make a good message broker do not necessarily make a good result store! For instance, while RabbitMQ is the best supported message broker, it should never be used as a result store since it will drop results after being asked for them once. Both Redis and Memcache are good candidates for result stores.
-     If you choose the same result store as message broker, you do not need to attach 2 add-ons. If not, make sure the result store add-on is attached.
-    """)
-markovmodel = MC(s)
+import time
 
-@app.route("/")
-def home():
-    #return histogram.sample()
-    return markovmodel.generate(10)
+def make_app():
+
+    def make_model():
+
+        def _make_model(corpus, n_sentences=10):
+            # global cv
+            def _generate():
+                markovchain = MC(corpus)
+                return markovchain.generate
+            generate = _generate()
+
+            child_conn.send(generate(n_sentences))
+
+            while True:
+                if parent_conn.poll():
+                    ## previously sent message got consumed
+                    # send another
+                    child_conn.send(generate(n_sentences))
+
+
+        parent_conn, child_conn = Pipe(duplex=True)
+
+        with open("res/the_adventures_of_sherlock_holmes.txt", "r") as f:
+            f_out = f.read()
+        make_process = Process(target=_make_model, args=(f_out,))
+        make_process.start()
+
+        return parent_conn, make_process
+
+    app = Flask(__name__)
+    parent_conn, make_process = make_model()
+
+    @app.route("/")
+    def home():
+        if parent_conn.poll():
+            return parent_conn.recv()
+        return "loading..."
+
+    return app
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app = make_app()
+    app.run(debug=True, port=8080)
